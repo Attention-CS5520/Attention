4d2f82fd4d59da659fa5d301883bc14c26bf09a5
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-07-25 14:01:00 -0400

    adding recylcerview

---
+++ app/src/main/java/edu/neu/numad21su/attention/MyItemCard.java
@@ -0,0 +1,48 @@
+package edu.neu.numad21su.attention;
+
+import edu.neu.numad21su.attention.ItemClickListener;
+
+public class MyItemCard implements ItemClickListener {
+
+    //private final String itemName;
+    private final String itemDesc;
+    private int imageSource;
+
+    // sender name = user name?
+    private String senderName;
+
+
+    //Constructor
+    public MyItemCard(int imageSource, String itemName, String itemDesc, boolean isChecked) {
+
+        //this.itemName = itemName;
+        this.itemDesc = itemDesc;
+        this.imageSource = imageSource;
+    }
+
+    //Getters for the imageSource, itemName and itemDesc
+
+
+    public String getItemDesc() {
+        return itemDesc;
+    }
+
+    //public String getItemName() {
+    //    return itemName;
+    //}
+
+    public int getImageSource() {
+        return imageSource;
+    }
+
+
+    @Override
+    public void onItemClick(int position) {
+
+    }
+
+    @Override
+    public void onCheckBoxClick(int position) {
+
+    }
+}
