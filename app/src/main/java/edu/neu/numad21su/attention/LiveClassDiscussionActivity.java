9ce1702ba952f78150ca4d381d36378f366bfc32
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-07-25 10:52:51 -0400

    added new activity for live class discussion

---
+++ app/src/main/java/edu/neu/numad21su/attention/LiveClassDiscussionActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.numad21su.attention;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LiveClassDiscussionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_class_discussion);
+    }
+}
