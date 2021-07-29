50fddf7229b73c16c0351f04ebf3813d656d519a
ColeStansbury <stansbury.c@northeastern.edu>
2021-07-28 22:38:28 -0400

    UIs complete

--- app/src/main/java/edu/neu/numad21su/attention/MainActivity.java
+++ app/src/main/java/edu/neu/numad21su/attention/MainActivity.java
@@ -1,14 +1,24 @@
 package edu.neu.numad21su.attention;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+  public void goToAttendance(View view) {
+    MainActivity.this.startActivity(new Intent(MainActivity.this, AttendanceActivity.class));
+  }
+
+  public void goToQuizResults(View view) {
+    MainActivity.this.startActivity(new Intent(MainActivity.this, QuizResults.class));
+  }
 }
