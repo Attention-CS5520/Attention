a6a1945d0ffc7f0ab99cafa358dcf59407b94cdb
sbj <sbahnj@gmail.com>
2021-08-04 11:12:51 -0400

    discussion excerpts fetched

--- app/src/main/java/edu/neu/numad21su/attention/InClass.java
+++ app/src/main/java/edu/neu/numad21su/attention/InClass.java
@@ -13,6 +13,9 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.installations.time.SystemClock;
 
 import android.content.Intent;
 import android.graphics.Color;
@@ -64,6 +67,7 @@ public class InClass extends AppCompatActivity {
 
     // The third discussion excerpt
     private TextView discussion3;
+
 
 
     @Override
@@ -153,28 +157,70 @@ public class InClass extends AppCompatActivity {
 
         TextView discussion1 = findViewById(R.id.InClassDiscussionText1);
 
-        DocumentReference post = db.collection("posts")
-                .document("L6yXHPcK6cg7v7mTG0Oc");
+        TextView discussion2 = findViewById(R.id.discussionText2);
 
-        post.get().addOnCompleteListener(new OnCompleteListener < DocumentSnapshot > () {
+        TextView discussion3 = findViewById(R.id.discussionText3);
+
+        DocumentReference post = db.collection("discussion_posts")
+                .document("user_post");
+
+        Query post_history = db.collection("discussion_posts").orderBy("date").limit(3);
+
+        post_history.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onComplete(@NonNull Task < DocumentSnapshot > task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot doc = task.getResult();
-                    StringBuilder fields = new StringBuilder("");
-                    fields.append("author: ").append(doc.get("author"));
-                    discussion1.setText(fields.toString());
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                if (task.isSuccessful()){
+
+                    QuerySnapshot querySnapshot = task.getResult();
+
+                    StringBuilder post1 = new StringBuilder("");
+                    StringBuilder post2 = new StringBuilder("");
+                    StringBuilder post3 = new StringBuilder("");
+
+                    post1.append(querySnapshot.getDocuments().get(0).get("message"));
+                    post2.append(querySnapshot.getDocuments().get(1).get("message"));
+                    post3.append(querySnapshot.getDocuments().get(2).get("message"));
+
+                    discussion1.setText(post1);
+                    discussion2.setText(post2);
+                    discussion3.setText(post3);
+
                 }
+
             }
-        })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
 
-                        Log.d("Failed", "item not read from firebase");
+                Log.d("discussions", "discussion history not found");
 
-                    }
-                });
+            }
+        });
+
+
+
+
+
+//        post.get().addOnCompleteListener(new OnCompleteListener < DocumentSnapshot > () {
+//            @Override
+//            public void onComplete(@NonNull Task < DocumentSnapshot > task) {
+//                if (task.isSuccessful()) {
+//                    DocumentSnapshot doc = task.getResult();
+//                    StringBuilder fields = new StringBuilder("");
+//                    fields.append(doc.get("message"));
+//                    discussion1.setText(fields.toString());
+//                }
+//            }
+//        })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//
+//                        Log.d("Failed", "item not read from firebase");
+//
+//                    }
+//                });
 
 
 
@@ -183,13 +229,17 @@ public class InClass extends AppCompatActivity {
     // A method to add a new discussion document to the post collection
     private void addPost(){
 
-        Map<String, Object> newPost = new HashMap<>();
-        newPost.put("author", "Bilbo Baggins");
-        newPost.put("classId", "classId");
-        newPost.put("message", "Time for tea");
-        newPost.put("subject", "Comments");
+        SystemClock clock = SystemClock.getInstance();
 
-        db.collection("discussion_posts").document("user_post")
+
+        Map<String, Object> newPost = new HashMap<>();
+        newPost.put("author", "Robert Lovelace");
+        newPost.put("classId", "classId");
+        newPost.put("message", "Here is my response");
+        newPost.put("subject", "Re: re: re:");
+        newPost.put("date", clock.currentTimeMillis());
+
+        db.collection("discussion_posts").document("user_post3")
                 .set(newPost).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(@NonNull Void unused) {
