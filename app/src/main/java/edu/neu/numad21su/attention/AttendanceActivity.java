d56e062fa6b0baba6fd674e9574b33e233b91446
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-08-08 07:43:58 -0400

    still trying to make audio recognition to work

--- app/src/main/java/edu/neu/numad21su/attention/AttendanceActivity.java
+++ app/src/main/java/edu/neu/numad21su/attention/AttendanceActivity.java
@@ -11,6 +11,7 @@ import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.speech.RecognizerIntent;
 import android.util.Log;
@@ -30,6 +31,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.Random;
@@ -181,6 +183,49 @@ public class AttendanceActivity extends AppCompatActivity {
 
           buttonStart.setEnabled(false);
           buttonStop.setEnabled(true);
+
+          //play pre-recorded audio here
+          /*
+          mediaPlayer = new MediaPlayer();
+          try {
+            mediaPlayer.setDataSource(view.getContext().getExternalFilesDir(null).getAbsolutePath() + "/" + "AudioRecording_music1.3gp");
+            mediaPlayer.prepare();
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+
+          mediaPlayer.start();
+
+
+           */
+
+
+
+          //
+          final Handler handler = new Handler();
+          handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+              if (mediaRecorder != null) {
+                try {
+                  afterRecording();
+                } catch(RuntimeException ex){
+                  //Ignore
+                }
+              }
+
+              if (mediaPlayer != null) {
+
+                  mediaPlayer.stop();
+                  mediaPlayer.release();
+
+              }
+
+            }
+          }, 4000);
+
+
+          //
 
           Toast.makeText(AttendanceActivity.this, "Recording started",
                   Toast.LENGTH_LONG).show();
@@ -477,20 +522,82 @@ public class AttendanceActivity extends AppCompatActivity {
 
   }
 
+  private List<String> extractCenter(String [] inputArray) {
+    List<String> outputList = new ArrayList<String>();
+    int idxSt = 0;
+    int idxEn = inputArray.length;
+
+    if (inputArray.length > 400) {
+      idxSt = 0;
+      idxEn = inputArray.length;
+    }
+
+
+
+    for (int i = idxSt; i < idxEn; i++) {
+      outputList.add(inputArray[i]);
+    }
+    return  outputList;
+  }
+
+  public String computePreFFT(String inputSignal) {
+
+    StringBuilder outputSignal = new StringBuilder();
+    String [] inputArray = inputSignal.split(" ");
+    //double output = 0.0;
+
+    for (int fo = 100; fo <= 3000; fo = fo + 100) {
+      for (int idx = 0; idx < inputArray.length; idx++) {
+        inputArray[idx]  = String.valueOf(Double.valueOf(inputArray[idx])
+                * Math.sin(2 * Math.PI * fo * idx * 0.000022675));
+
+      }
+    }
+    //X[n] = Math.sin(2*Math.PI*fo*n*ts)
+
+    for (int idx = 0; idx < inputArray.length; idx++) {
+      outputSignal.append(inputArray[idx]);
+      outputSignal.append(" ");
+    }
+
+
+
+    return outputSignal.toString();
+  }
+
+  private double getSum(String inputStr) {
+    double allSum = 0;
+    String [] inputArr = inputStr.split(" ");
+    for (int i = 0; i < inputArr.length; i ++) {
+      allSum += Double.valueOf(inputArr[i]);
+    }
+    return allSum;
+  }
+
   public double computeFFT(String model, String test) {
-    String [] modelParsed = model.split(" ");
-    String [] testParsed = test.split(" ");
+    List<String> modelParsed = new ArrayList<String>();
+    List<String> testParsed = new ArrayList<String>();
+
+    String [] modelParsedArray = model.split(" ");
+    String [] testParsedArray = test.split(" ");
+
+    modelParsed = extractCenter(modelParsedArray);
+    testParsed = extractCenter(testParsedArray);
+
+
     //Log.i("debugParsed",modelParsed[10]);
     //Log.i("debugParsed",modelParsed[10]);
+    Log.d("length of old model file",String.valueOf(modelParsed.size()));
+    Log.d("length of file",String.valueOf(testParsed.size()));
 
     int idx = 0;
     double sumOfInnerProduct = 0.0;
     double finalSumOfInnerProduct = -999999999999.0;
-    while (idx < modelParsed.length) {
+    while (idx < modelParsed.size()) {
       int jdx = 0;
       int jdxModel = idx;
-      while (jdx < testParsed.length && jdxModel < modelParsed.length) {
-        sumOfInnerProduct += (Double.valueOf(testParsed[jdx]) * Double.valueOf(modelParsed[jdxModel]));
+      while (jdx < (testParsed.size()/2) && jdxModel < modelParsed.size()) {
+        sumOfInnerProduct += (Double.valueOf(testParsed.get(jdx)) * Double.valueOf(modelParsed.get(jdxModel)));
         jdx++;
         jdxModel++;
       }
@@ -499,23 +606,13 @@ public class AttendanceActivity extends AppCompatActivity {
       }
       sumOfInnerProduct = 0.0;
       idx++;
-      break;
+      //break;
     }
     return finalSumOfInnerProduct;
   }
 
-  public double[] windowFunction(int nSamples) {
-    int m = nSamples / 2;
-    double r;
-    double pi = Math.PI;
-    double[] w = new double[nSamples];
-    r = pi / m;
-    for (int n = -m; n < m; n++)
-      w[m + n] = 0.54f + 0.46f * Math.cos(n * r);
-    return w;
-  }
-
-  public double[] filterOut(String inputArray) {
+  public String filterOut(String inputArray) {
+    Log.d("started debug", "DEBUG");
     String [] inputArrayString = inputArray.split(" ");
     double [] doubleArray = new double[inputArrayString.length];
 
@@ -523,7 +620,8 @@ public class AttendanceActivity extends AppCompatActivity {
       doubleArray[idx] = Double.valueOf(inputArrayString[idx]);
     }
 
-    HighPassFilter filter = new HighPassFilter(100,44100, HighPassFilter.PassType.Highpass,1);
+    //HighPassFilter filter = new HighPassFilter(100,44100, HighPassFilter.PassType.Highpass,1);
+    HighPassFilter filter = new HighPassFilter(50,44100, HighPassFilter.PassType.Highpass,1);
     StringBuilder outputSb = new StringBuilder();
     for (int i = 0; i < doubleArray.length; i++)
     {
@@ -531,7 +629,6 @@ public class AttendanceActivity extends AppCompatActivity {
       doubleArray[i] = filter.getValue();
       outputSb.append(filter.getValue());
       outputSb.append(" ");
-      //Log.d("each data", String.valueOf(filter.getValue()));
     }
     // Lets print
 
@@ -544,11 +641,197 @@ public class AttendanceActivity extends AppCompatActivity {
       end = end > letsPrint.length() ? letsPrint.length() : end;
 
 
-      Log.d("hello saved long data", letsPrint.substring(start, end));
+      //Log.d("hello saved long data", letsPrint.substring(start, end));
     }
 
-    Log.d("print whole filterd", letsPrint);
-    return doubleArray;
+    return letsPrint;
+  }
+
+  public void afterRecording() {
+    if (mediaRecorder != null) {
+      try {
+        mediaRecorder.stop();
+        mediaRecorder.release();
+      } catch(RuntimeException ex){
+        //Ignore
+      }
+    }
+    //mediaRecorder.stop();
+    buttonStop.setEnabled(false);
+    buttonPlayLastRecordAudio.setEnabled(true);
+    buttonStart.setEnabled(true);
+    buttonStopPlayingRecording.setEnabled(false);
+
+    Toast.makeText(AttendanceActivity.this, "Recording Completed",
+            Toast.LENGTH_LONG).show();
+
+    // printing binary
+    try {
+      byte[] encoded = Files.readAllBytes(Paths.get(AudioSavePathInDevice));
+      Log.i("audio string",Arrays.toString(encoded));
+    } catch (IOException e) {
+
+    }
+
+    // try in decimal
+    StringBuilder sb = new StringBuilder();
+    int maxNum = 0;
+    try (BufferedInputStream is = new BufferedInputStream(new FileInputStream(AudioSavePathInDevice))) {
+      for (int i; (i = is.read()) != -1;) {
+        //String temp = Integer.toHexString(i).toUpperCase();
+        String temp = Integer.toString(i).toUpperCase();
+        //if (temp.length() == 1) {
+        //  sb.append('0');
+        //}
+        sb.append(temp).append(' ');
+        if (Integer.valueOf(temp) > maxNum) maxNum = Integer.valueOf(temp);
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    Log.i("hex output",sb.toString());
+    Log.i("maxVal",String.valueOf(maxNum));
+    Log.i("size",String.valueOf(sb.length()));
+    Log.i("file location", AudioSavePathInDevice);
+    //File myObj = new File("/storage/emulated/0/Android/data/edu.neu.numad21su.attention/files/OMIFGAudioRecording.3gp");
+
+    //myObj.delete();
+
+    // Modify the values in string buffer to normalize and change polarities if needed
+    StringBuffer sbNorm = new StringBuffer();
+    String[] sbArray = sb.toString().split(" ");
+    for (int idx =0; idx < sbArray.length; idx++) {
+      double tempDouble = ((double)((double) Integer.valueOf(sbArray[idx]) /maxNum)) - 0.5;
+      sbNorm.append(String.valueOf(tempDouble));
+      sbNorm.append(" ");
+
+    }
+
+
+    try {
+
+      View view = findViewById(android.R.id.content);
+      File root = new File(view.getContext().getExternalFilesDir(null), "Notes");
+      if (!root.exists()) {
+        Log.i("file creation","I am trying to create directory");
+        root.mkdirs();
+      }
+
+      if (!root.exists()) {
+        Log.i("file creation","Could not create a directory!!");
+      }
+
+      File gpxfile = new File(root, "inputfile");
+      //File gpxfile = new File(root, "hello");
+      gpxfile.delete();
+      FileWriter writer = new FileWriter(gpxfile);
+      writer.append(sbNorm.toString());
+      writer.flush();
+      writer.close();
+      //Reading
+      Scanner myReader = new Scanner(gpxfile);
+
+      String data = myReader.nextLine();
+      Log.i("Reading file data",data);
+      myReader.close();
+      // create test for hello
+
+      gpxfile = new File(root, "music7");
+      myReader = new Scanner(gpxfile);
+      String a = myReader.nextLine();
+      int maxLogSize = 900;
+      for(int i = 0; i <= a.length() / maxLogSize; i++) {
+        int start = i * maxLogSize;
+        int end = (i + 1) * maxLogSize;
+        end = end > a.length() ? a.length() : end;
+
+
+        //Log.d("good saved long data", hello.substring(start, end));
+      }
+
+      // apply filter on hello data
+      filterOut(a);
+
+      //}
+      myReader.close();
+
+      gpxfile = new File(root, "music1");
+      myReader = new Scanner(gpxfile);
+      String b = myReader.nextLine();
+
+      gpxfile = new File(root, "music6");
+      myReader = new Scanner(gpxfile);
+      String c = myReader.nextLine();
+
+      //double helloTest = computeFFT(computePreFFT(filterOut(a)),computePreFFT(filterOut(data)));
+      double aTest = computeFFT(filterOut(a),filterOut(data));
+      //double aTest = computeFFT(filterOut(data),filterOut(a));
+      Log.d("uuuuuuuuuuuu", "!!!!!!!!!!!!!!!!!!!");
+      Log.d("a Test result", String.valueOf(aTest));
+
+      double bTest = computeFFT(filterOut(b),filterOut(data));
+      //double bTest = computeFFT(filterOut(data),filterOut(b));
+      //Log.d("uuuuuuuuuuuu", "!!!!!!!!!!!!!!!!!!!");
+      Log.d("b Test result", String.valueOf(bTest));
+
+      double cTest = computeFFT(filterOut(c),filterOut(data));
+      //double cTest = computeFFT(filterOut(data),filterOut(c));
+      //Log.d("uuuuuuuuuuuu", "!!!!!!!!!!!!!!!!!!!");
+      Log.d("c Test result", String.valueOf(cTest));
+
+
+      double aVal = getSum(computePreFFT(filterOut(a)));
+      double bVal = getSum(computePreFFT(filterOut(b)));
+      double cVal = getSum(computePreFFT(filterOut(c)));
+      double inputVal = getSum(computePreFFT(filterOut(data)));
+      //double aVal = getSum(computePreFFT((a)));
+      //double bVal = getSum(computePreFFT((b)));
+      //double cVal = getSum(computePreFFT((c)));
+      //double inputVal = getSum(computePreFFT((data)));
+      //Log.d("helloSum", String.valueOf(getSum(computePreFFT(filterOut(hello)))));
+
+      //Log.d("helloSum", String.valueOf(Math.abs(inputVal - aVal)));
+      //Log.d("goodSum", String.valueOf(Math.abs(inputVal - bVal)));
+      //Log.d("classSum", String.valueOf(Math.abs(inputVal - cVal)));
+
+      //Log.d("helloSum", String.valueOf(getSum(computePreFFT((hello)))));
+      //Log.d("goodSum", String.valueOf(getSum(computePreFFT((good)))));
+      //Log.d("classSum", String.valueOf(getSum(computePreFFT((class_)))));
+      //Log.d("inputSum", String.valueOf(getSum(computePreFFT((data)))));
+
+
+          /*
+          // create test for good
+          gpxfile = new File(root, "good");
+          myReader = new Scanner(gpxfile);
+          //while (myReader.hasNextLine()) {
+          String good = myReader.nextLine();
+          Log.i("Reading file data",good);
+          //}
+          myReader.close();
+          double goodTest = computeFFT(filterOut(good),filterOut(data));
+          Log.i("Good Test result", String.valueOf(goodTest));
+
+
+          // create test for class
+          gpxfile = new File(root, "class");
+          myReader = new Scanner(gpxfile);
+          //while (myReader.hasNextLine()) {
+          String class_ = myReader.nextLine();
+          Log.i("Reading file data",class_);
+          //}
+          myReader.close();
+          double classTest = computeFFT(filterOut(class_),filterOut(data));
+          Log.i("Class Test result", String.valueOf(classTest));
+          */
+
+      Toast.makeText(AttendanceActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
   }
 }
 
