6f668b4407bfa191a2612b0c703ac0dbc8bccf4e
ColeStansbury <stansbury.c@northeastern.edu>
2021-08-09 23:14:08 -0400

    Quiz manager working

--- app/src/main/java/edu/neu/numad21su/attention/QuizEditor.java
+++ app/src/main/java/edu/neu/numad21su/attention/QuizEditor.java
@@ -1,30 +1,45 @@
 package edu.neu.numad21su.attention;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import edu.neu.numad21su.attention.quizScreen.Quiz;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.TextView;
 
 public class QuizEditor extends AppCompatActivity {
   private RecyclerView recyclerView;
   private QuestionRecyclerAdapter rviewAdapter;
   private RecyclerView.LayoutManager rLayoutManger;
   private Quiz quiz;
+  private FirebaseFirestore db;
+  private EditText quizTitle;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_quiz_editor);
     this.quiz = (Quiz) this.getIntent().getSerializableExtra("Quiz");
-    if(quiz != null){
-      ((TextView)this.findViewById(R.id.quiz_editor_title)).setText(quiz.getQuizTitle());
-      ((TextView)this.findViewById(R.id.quiz_editor_last_edited)).setText(quiz.getLastEdited());
+    if (quiz != null) {
+      ((TextView) this.findViewById(R.id.quiz_editor_title)).setText(quiz.getQuizTitle());
+      ((TextView) this.findViewById(R.id.quiz_editor_last_edited)).setText(quiz.getLastEdited());
     }
+    db = FirebaseFirestore.getInstance();
     createRecyclerView();
+    quizTitle = findViewById(R.id.quiz_editor_title);
   }
 
   private void createRecyclerView() {
@@ -43,5 +58,18 @@ public class QuizEditor extends AppCompatActivity {
     });
     recyclerView.setAdapter(rviewAdapter);
     recyclerView.setLayoutManager(rLayoutManger);
+  }
+
+  public void saveQuiz(View view) {
+    quiz.setQuizTitle(quizTitle.getText().toString());
+    quiz.setLastEdited(new SimpleDateFormat("yyyy-MM-dd H:mm aaa").format(new Date()));
+    if(quiz.quizId == null){
+      quiz.quizId = UUID.randomUUID().toString();
+    }
+    db.collection("quizzes").document(quiz.getQuizId()).set(quiz, SetOptions.merge())
+            .addOnSuccessListener(dr -> {
+              Toast.makeText(QuizEditor.this, "Saved!", Toast.LENGTH_SHORT).show();
+              QuizEditor.this.finish();
+            }).addOnFailureListener(e -> Log.d("error", e.toString()));
   }
 }
--- app/src/main/java/edu/neu/numad21su/attention/QuizManager.java
+++ app/src/main/java/edu/neu/numad21su/attention/QuizManager.java
@@ -1,7 +1,12 @@
 package edu.neu.numad21su.attention;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,17 +26,20 @@ public class QuizManager extends AppCompatActivity {
           Arrays.asList(
                   new Quiz("1", "1", "Pointers versus Variables", "10/10/2020",
                            new ArrayList<>(Arrays.asList(new Question("Pick One", "A", "B",
-                                                                      "C","D", "C")))),
+                                                                      "C", "D", "C")))),
                   new Quiz("1", "2", "Loops", "1/1/2001", new ArrayList<>()),
                   new Quiz("1", "3", "Memory Management", "12/25/2021", new ArrayList<>()),
                   new Quiz("1", "4", "Recursion", "3/3/2003", new ArrayList<>())
           ));
+  private FirebaseFirestore db;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_quiz_manager);
     createRecyclerView();
+    db = FirebaseFirestore.getInstance();
+    getQuizzes();
   }
 
   private void createRecyclerView() {
@@ -39,16 +47,46 @@ public class QuizManager extends AppCompatActivity {
     recyclerView = findViewById(R.id.quizCardRecycler);
     recyclerView.setHasFixedSize(true);
     rviewAdapter = new QuizRecyclerAdapter(quizCards);
-    rviewAdapter.setDeleteItemListener(i -> {
-      quizCards.remove(i);
-      rviewAdapter.notifyDataSetChanged();
-    });
-    rviewAdapter.setEditItemListener(i -> {
-      Intent editIntent = new Intent(QuizManager.this, QuizEditor.class);
-      editIntent.putExtra("Quiz", quizCards.get(i));
-      QuizManager.this.startActivity(editIntent);
-    });
+    rviewAdapter.setDeleteItemListener(i -> deleteQuiz(quizCards.get(i)));
+    rviewAdapter.setEditItemListener(i -> openQuiz(quizCards.get(i)));
     recyclerView.setAdapter(rviewAdapter);
     recyclerView.setLayoutManager(rLayoutManger);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    getQuizzes();
+  }
+
+  private void getQuizzes() {
+    db.collection("quizzes").get()
+            .addOnSuccessListener(dr -> {
+              quizCards.clear();
+              quizCards.addAll(dr.toObjects(Quiz.class));
+              rviewAdapter.notifyDataSetChanged();
+            })
+            .addOnFailureListener(e -> Log.d("error", e.toString()));
+  }
+
+  private void openQuiz(Quiz quiz) {
+    Intent editIntent = new Intent(QuizManager.this, QuizEditor.class);
+    editIntent.putExtra("Quiz", quiz);
+    QuizManager.this.startActivity(editIntent);
+  }
+
+  public void createQuiz(View view) {
+    Quiz quiz = new Quiz();
+    quizCards.add(quiz);
+    openQuiz(quiz);
+  }
+
+  private void deleteQuiz(Quiz quiz) {
+    db.collection("quizzes").document(quiz.quizId).delete()
+            .addOnSuccessListener(dr -> {
+              quizCards.remove(quiz);
+              rviewAdapter.notifyDataSetChanged();
+              Toast.makeText(QuizManager.this, "Deleted.", Toast.LENGTH_SHORT).show();
+            }).addOnFailureListener(e -> Log.d("error", e.toString()));
   }
 }
