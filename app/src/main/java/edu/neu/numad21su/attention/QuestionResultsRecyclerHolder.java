7360b31aeafbebb8b5ed8d313d333c295849a02e
richardcastaneda <61243707+richardcastaneda@users.noreply.github.com>
2021-08-13 22:39:06 -0700

    Fixed Recycler view and edited Graph, need to implement graph logic

--- app/src/main/java/edu/neu/numad21su/attention/QuestionResultsRecyclerHolder.java
+++ app/src/main/java/edu/neu/numad21su/attention/QuestionResultsRecyclerHolder.java
@@ -1,14 +1,21 @@
 package edu.neu.numad21su.attention;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class QuestionResultsRecyclerHolder extends RecyclerView.ViewHolder {
     public TextView preview;
+    public TextView answer;
+    public ImageView incorrect;
+    public ImageView correct;
 
-    public QuestionResultsRecyclerHolder(View view, ItemClickListener editItemListener, ItemClickListener deleteItemListener) {
+    public QuestionResultsRecyclerHolder(View view) {
         super(view);
-        preview = itemView.findViewById(R.id.question_preview);
+        preview = itemView.findViewById(R.id.quizResults_question_text);
+        answer = itemView.findViewById(R.id.quizResults_answer);
+        incorrect = itemView.findViewById(R.id.incorrect_indicator);
+        correct = itemView.findViewById(R.id.correct_indicator);
     }
 }
